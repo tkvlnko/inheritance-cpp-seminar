a04b1ced22abd5a4ede7154cba4b4d654b58bbee
Taya <tayaibragimova@gmail.com>
2024-03-13 13:31:02 +0300

    add main.cpp

--- main.cpp
+++ main.cpp
@@ -1,88 +1,93 @@
 #include <iostream>
+#include <utility>
+#include <vector>
 
-class Person
+class Shape
 {
-    std::string name;
-
 public:
-    Person(const std::string&);
+    Shape(double = 0, double = 0);
+    virtual double getArea() = 0;// каждый доверний класс обязан реализовать иначе CE
+
+    //virtual функцию можно переопределить в дочернем классе
+    // override (в дочернем) -- явно указываем что будем перезаписывать
     virtual void print() const;
+    std::pair<double, double> getCoords() const;
+
+private:
+    std::pair<double, double> coord;
 };
 
-Person::Person(const std::string& name)
+void Shape::print() const
 {
-    this->name = name;
+    std::cout << "this is shape:  " << coord.first << '\t' << coord.second << '\n';
 }
 
-void Person::print() const
-{
-    std::cout << this->name << '\n';
-}
-
-class Teacher : public Person
+class Square : public Shape
 {
 private:
-    double salary;
+    double side;
 
 public:
-    Teacher(const std::string&, double);
-    void print() const;
+    Square(std::pair<double, double>, double);
+    void print() const override;
+    double getArea() override;
 };
 
-Teacher::Teacher(const std::string& name, double salary) : Person(name), salary(salary)
+Shape::Shape(double x, double y)
 {
+    this->coord = std::make_pair(x, y);
 }
 
-void Teacher::print() const
+double Square::getArea()
 {
-    Person::print();
-    std::cout << this->salary << '\n';
-};
+    return side * side;
+}
 
-class Student : public Person
+std::pair<double, double> Shape::getCoords() const
 {
-private:
-    int rating;
+    return this->coord;
+}
 
-public:
-    Student(const std::string&, int);
-    void print() const;
-};
-
-Student::Student(const std::string& name, int rating) : Person(name), rating(rating){};
-
-
-void Student::print() const
+Square::Square(std::pair<double, double> coords, double side) : Shape(coords.first, coords.second)
 {
-    Person::print();
-    std::cout << this->rating << '\n';
-};
+    // часть конструктора реализуется по наследованию
+    this->side = side;
+}
 
-
-class Assistant : public Student, public Teacher
+void Square::print() const
 {
-
-public:
-    Assistant(const std::string&, double, int);
-    void print() const;
-};
-
-Assistant::Assistant(const std::string& name, double salary, int rating) : Teacher(name, salary), Student(name, rating)
-{}
-
-
-void Assistant::print() const
-{
-    Teacher::print();
-    Student::print();
-};
-
-
+    std::cout << "this is square:  " << this->getCoords().first << '\t' << this->getCoords().second << '\n';
+}
 int main()
 {
-    Person ivan{"ivan"};
-    Teacher petro{"petro", 1000000};
-    Student ivan2{"ivan2", 1};
+    // Shape s{1, 1}; // не можем создать тк не реализована getArea()
 
-    ivan2.print();
+    Square sq{{2, 2}, 2};
+    // s.print();
+    // sq.print();
+
+    // std::unique_ptr<Shape> = std::make_unique(Square {{2, 2}, 2});
+    Shape* sqPtr = new Square{{2, 2}, 3};
+
+    // Shape* sPtr = new Shape{2, 3};
+
+    sqPtr->print();
+    // sPtr->print();
+
+    // std::vector<Shape*> vecOfShapes{sqPtr, sPtr};
+
+    // for (int i = 0; i < 2; i++)
+    // {
+    //     vecOfShapes[i]->print();
+    // }
+
+
+    // // нужно чтобы не делать delete, он сделает это сам когда выйдет из зоны видимости
+    // std::unique_ptr<int> a = std::make_unique<int>(5);
+    // std::unique_ptr<int> a2;
+    // // a2 = a; // так не можем
+
+    // std::shared_ptr<int> b = std::make_shared<int>(6);
+    // std::shared_ptr<int> b2;
+    // b2 = b;
 }
